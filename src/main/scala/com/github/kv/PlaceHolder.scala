2469680f35ab069f63471caa966d416d2531ed3f
Kumar Vaibhav <kvaibhav@optum.com>
2020-10-03 15:23:54 -0500

    Some initialc changes

---
+++ src/main/scala/com/github/kv/PlaceHolder.scala
@@ -0,0 +1,5 @@
+package com.github.kv
+
+class PlaceHolder {
+
+}
---
+++ src/main/scala/com/github/kv/datafunctions/RegexBased.scala
@@ -0,0 +1,10 @@
+package com.github.kv.datafunctions
+
+class RegexBased {
+
+}
+
+we need to define something similar to handlers!!
+
+each handler will have stuff specific to it - like min and mx for numeric data, and string for regex, but they will all have things similar as well -
+like number of records to generate, null ratio, etc.
